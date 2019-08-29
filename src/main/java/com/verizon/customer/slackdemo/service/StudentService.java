a20a6f6a1839d85bc3376018e2bb4ee855eaa1cb
gautam <gautamk1921@gmail.com>
2019-08-29 16:49:55 +0530

    service added

---
+++ src/main/java/com/verizon/customer/slackdemo/service/StudentService.java
@@ -0,0 +1,57 @@
+package com.verizon.customer.slackdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.verizon.customer.slackdemo.model.Student;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	StudentJmsSender jmsStudent;
+
+	@Autowired
+	private StudentRepository repository;
+
+	@Transactional(readOnly = true)
+	public Student getStudent(int id) {
+
+		return repository.getOne(id);
+
+	}
+
+	@Transactional
+	public Student save(Student obj) throws Exception {
+		Student persitedObject = repository.save(obj);
+		jmsStudent.pushStudentSave(persitedObject);
+		return persitedObject;
+	}
+
+	@Transactional
+	public Student update(int id,Student obj) throws Exception {
+		obj.setId(id);
+		Student persitedObject= repository.save(obj);
+		jmsStudent.pushStudentSave(persitedObject);
+		return persitedObject;
+	}
+
+	@Transactional
+	public boolean delete(int id) throws Exception {
+		repository.deleteById(id);
+		jmsStudent.pushStringMessage("deleted Student with Id=" + 1);
+		return true;
+	}
+	
+	public List<Student> findByNameOrAge(String name,int age){
+		return repository.findByNameOrAge(name, age);
+	}
+
+}
