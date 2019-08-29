6051988d200422794f11eef7c513f2747368510a
varshini <varshinikaithapuram@gmail.com>
2019-08-29 15:08:46 +0530

    new commit

---
+++ src/main/java/com/verizon/customer/slackdemo/AppConfiguration.java
@@ -0,0 +1,27 @@
+package com.verizon.customer.slackdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/***
+ *
+ * I have added the swagger into the project slack demo
+ *
+ */
+
+@Configuration
+
+public class AppConfiguration {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
+
+}
